d703dd668055e47ecd9ed3099a1674fa8e35c15a
onkartripathi15 <tripathi.onkar@thinksys.com>
2022-06-24 10:39:09 +0530

    added pagination

--- Models/Details.cs
+++ Models/Details.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 // Code scaffolded by EF Core assumes nullable reference types (NRTs) are not used or disabled.
 // If you have enabled NRTs for your project, then un-comment the following line:
@@ -10,14 +11,24 @@ namespace LostOrFound.Models
     public partial class Details
     {
         public int UserId { get; set; }
+        [Required(ErrorMessage = "Please enter First Name.")]
         public string FirstName { get; set; }
+        [Required(ErrorMessage = "Please enter LastName.")]
         public string LastName { get; set; }
+        [Required]
         public string ItemDetails { get; set; }
+        [Required]
         public string Contact { get; set; }
+        [Required]
         public string City { get; set; }
+        [Required]
         public string State { get; set; }
+        [Required]
         public string Status { get; set; }
+        [Required]
         public string Date { get; set; }
+        [Required]
         public string Time { get; set; }
+      
     }
 }
